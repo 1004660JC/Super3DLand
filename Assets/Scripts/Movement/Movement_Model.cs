d4d3d2b35de1c83bfaaf326d682e61b28c32831e
Unknown <1004660@student.sae.edu.au>
2018-02-10 01:53:52 +1100

    Added basic movement model and view scripts

---
+++ Assets/Scripts/Movement/Movement_Model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Model : MonoBehaviour
+{
+	public String AxisNameH;
+	public String AxisNameV;
+
+	public float AxisInputH;
+	public float AxisInputV;
+
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		AxisNameH = "H";
+		AxisNameV = "V";
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		AxisInputH = Input.GetAxis(AxisNameH);
+		AxisInputV = Input.GetAxis(AxisNameV);
+	}
+}
---
+++ Assets/Scripts/Movement/Movement_View.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_View : MonoBehaviour
+{
+
+	public Movement_Model MM;
+	public int Speed;
+
+	void OnEnable()
+	{
+		MM = GetComponent<Movement_Model>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(MM.AxisInputH * Speed, 0, MM.AxisInputV * Speed);
+	}
+}
